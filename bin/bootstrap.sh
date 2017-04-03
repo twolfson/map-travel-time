5d14664a405371a1a7f225e3270437f7829aeff2
Todd Wolfson <todd@twolfson.com>
2017-04-03 02:52:20 -0700

    Fixed up bootstrapping

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -9,6 +9,8 @@ mkdir -p vendor
 cd vendor
 if ! test -f sfmta-60.zip; then
   wget http://transitfeeds.com/p/sfmta/60/latest/download --output-document sfmta-60.zip
-  unzip sfmta-60.zip
+  unzip sfmta-60.zip -d sfmta-60
+  md5sum sfmta-60.zip > sfmta-60/MD5SUM
+  date --utc > sfmta-60/TIMESTAMP
 fi
 cd -
