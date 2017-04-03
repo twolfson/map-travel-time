c64fd9640013a4a32a7be8e967aa22eaa1094dd4
Todd Wolfson <todd@twolfson.com>
2017-04-02 17:35:43 -0700

    Verified tests working

---
+++ test/browser/map-travel-time.js
@@ -0,0 +1,9 @@
+// Load in dependencies
+var assert = require('assert');
+
+// Start our tests
+describe('An assertion', function () {
+  it('is valid', function () {
+    assert.strictEqual(2, 2);
+  });
+});
