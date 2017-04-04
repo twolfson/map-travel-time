71430707a11e757a844b5b95596cdfd8ba1683a0
Todd Wolfson <todd@twolfson.com>
2017-04-03 23:55:24 -0700

    Grabbed our data

--- tmp.js
+++ tmp.js
@@ -1,6 +1,27 @@
 // Load in our dependencies
 var fs = require('fs');
-var readVendorFile = function (filepath) { return fs.readFileSync(__dirname + '/../vendor/' + filepath, 'utf8').trim(); };
+var Papa = require('papaparse');
+
+// Define our helpers
+// TODO: Polish our helpers
+function parseCsvStr(csvStr) {
+  var results = Papa.parse(csvStr, {
+    header: true
+  });
+  if (results.errors.length) {
+    console.error('Error encountered', results.errors[0]);
+    throw new Error(results.errors[0].message);
+  }
+  return results.data;
+}
+function readVendorFile(filepath) {
+  return fs.readFileSync(__dirname + '/vendor/' + filepath, 'utf8').trim();
+}
 
 // Load in our stop data
 var csvStopTimesStr = readVendorFile('sfmta-60/stop_times.txt');
+console.log('start...');
+var stopTimes = parseCsvStr(csvStopTimesStr);
+console.log('stop');
+
+console.log(stopTimes.slice(0, 10));
