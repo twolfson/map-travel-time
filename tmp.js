f5a356c21d6c2738ec01117720f8570465198986
Todd Wolfson <todd@twolfson.com>
2017-04-04 02:03:18 -0700

    Added data generation for time diff

--- tmp.js
+++ tmp.js
@@ -1,6 +1,8 @@
 // Load in our dependencies
 var _ = require('underscore');
+var assert = require('assert');
 var fs = require('fs');
+var moment = require('moment');
 var Papa = require('papaparse');
 
 // Define our helpers
@@ -52,7 +54,7 @@ var stopTimes = readVendorFile(__dirname + '/vendor/sfmta-60/stop_times.txt');
 console.log('File loaded');
 
 // TODO: Remove DEV slicing
-// stopTimes = stopTimes.slice(0, 10);
+stopTimes = stopTimes.slice(0, 10);
 
 // Group our trip datas by their id
 var stopTimesMapByTripId = _.groupBy(stopTimes, function groupByTripId (stopTime) {
@@ -61,15 +63,47 @@ var stopTimesMapByTripId = _.groupBy(stopTimes, function groupByTripId (stopTime
 
 // Strip away data we don't need
 // DEV: We use a similar compression to Google Calendar's timezones
-//   i.e. ['trip_id', ['stop_id', 'timestamp']]
+//   i.e. ['trip_id', ['stop_id', 'time_to_next_stop']]
 //   This reduces us from 260kb gzipped to 170kb gzipped
 //   Due to sheer number of repeated keys/references
 var VALID_KEYS = ['stop_id'];
 var retArr = _.map(stopTimesMapByTripId, function iterateStopTimeArrs (stopTimeArr, tripId) {
+  // Extract our first arrival time for serialization
+  var firstStop = stopTimeArr[0];
+  assert(firstStop);
+  var firstArrivalTime = firstStop.arrival_time;
+  assert(firstArrivalTime);
+
+  // Generate our return data
   return [
     tripId,
-    stopTimeArr.map(function stripStopTimesData (stopTime) {
-      return _.values(_.pick(stopTime, VALID_KEYS));
+    stopTimeArr[0].arrival_time,
+    stopTimeArr.map(function stripStopTimesData (stopTime, i) {
+      // Assume trips are circular
+      // TODO: Figure out circular trip detection
+      var nextStop = stopTimeArr[i + 1] || stopTimeArr[0];
+      assert(nextStop);
+
+      // Parse stop info
+      // https://momentjs.com/docs/#/parsing/string-format/
+      var currentDepartureTimeStr = stopTime.departure_time;
+      assert(currentDepartureTimeStr);
+      var currentDepartureTime = moment('1970-01-01T' + currentDepartureTimeStr + 'Z');
+      assert(currentDepartureTime.isValid(), 'Time isn\'t valid: 1970-01-01T' + currentDepartureTimeStr + 'Z');
+
+      var nextArrivalTimeStr = stopTime.arrival_time;
+      assert(nextArrivalTimeStr);
+      var nextArrivalTime = moment('1970-01-01T' + nextArrivalTimeStr + 'Z');
+      assert(nextArrivalTime.isValid());
+
+      // Resolve time to our next stop
+      assert(currentDepartureTime.isBefore(nextArrivalTime));
+      var timeToNextStop = nextArrivalTime - currentDepartureTime;
+
+      // Generate and return our data
+      var values = _.values(_.pick(stopTime, VALID_KEYS));
+      assert.strictEqual(values.length, VALID_KEYS.length);
+      return values.concat([timeToNextStop]);
     })
   ];
 });
