9a88b489958c6196b536469e6559de8a94ca577e
Todd Wolfson <todd@twolfson.com>
2017-04-03 23:20:52 -0700

    Moved to dynamic data

--- browser/js/index.js
+++ browser/js/index.js
@@ -22,29 +22,18 @@ function parseCsvStr(consoleLabel, csvStr) {
 // Define our Application constructor
 function Application(params) {
   // Verify we have our parameters
-  assert(params.csvStopsStr);
   assert(params.el);
-
-  // Parse our CSV info
-  var stopInfoArr = parseCsvStr('Parsing stops', params.csvStopsStr);
-  var stopTimesInfoArr = parseCsvStr('Parsing stop times', params.csvStopTimesStr);
-  var tripInfoArr = parseCsvStr('Parsing trips', params.csvTripsStr);
-
-  // Slice our stop data for development
-  // TODO: Remove dev edit
-  // stopInfoArr = stopInfoArr.slice(0, 10);
 
   // Bind Leaflet to our element
   // http://leafletjs.com/reference-1.0.3.html#map-factory
   // TODO: Determine good starting point for each city
   //   Maybe make SF Bay Area multiple locations with same files
   //   So SF and Oakland all start in nice points
-  var map = L.map(params.el, {
-    center: [stopInfoArr[0].stop_lat, stopInfoArr[0].stop_lon],
+  var map = this.map = L.map(params.el, {
     zoom: 13
   });
 
-  // Define our tile layer
+  // Define our layers
   // DEV: We are using OpenStreetmap as inspired by Mapnificent. No need to use Mapbox
   // https://switch2osm.org/using-tiles/getting-started-with-leaflet/
   var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -53,26 +42,11 @@ function Application(params) {
     maxZoom: 18
   });
   osmLayer.addTo(map);
-
-  // Draw dots for each of our stations
-  // TODO: Make stations debuggable so group them in a layer or something
-  var stopMarkers = stopInfoArr.map(function generateStopMarker (stopInfo) {
-    // [{stop_id: '98', stop_code: '198', stop_name: '2ND ST ...', stop_desc: ' ',
-    //   stop_lat: '37...', stop_lon: '-122...', zone_id: ' ', stop_url: ' '}, ...]
-    // to '2ND ST ... (id: 98, code: 198)'
-    var popupStr = stopInfo.stop_name + ' (id: ' + escapeHtml(stopInfo.stop_id) + ', ' +
-      'code: ' + escapeHtml(stopInfo.stop_code) + ')';
-    return L.circleMarker([stopInfo.stop_lat, stopInfo.stop_lon], {
-      stroke: false,
-      fillOpacity: 1.0,
-      radius: 3
-    }).bindPopup(popupStr);
-  });
-  var stopMarkersLayer = L.layerGroup(stopMarkers);
   // TODO: Disable stops layer after development complete
+  var stopMarkersLayer = this.stopMarkersLayer = L.layerGroup([]);
   stopMarkersLayer.addTo(map);
 
-  // Bind our layers to our map
+  // Bind controls for our layers
   // http://leafletjs.com/examples/layers-control/
   // http://leafletjs.com/reference.html#control-layers
   L.control.layers({
@@ -106,4 +80,39 @@ function Application(params) {
     lastClickTime = now;
   });
 }
+Application.prototype = {
+  loadData: function (params) {
+    // Parse our CSV info
+    var stopInfoArr = parseCsvStr('Parsing stops', params.csvStopsStr);
+    // var stopTimesInfoArr = parseCsvStr('Parsing stop times', params.csvStopTimesStr);
+    // var tripInfoArr = parseCsvStr('Parsing trips', params.csvTripsStr);
+
+    // Slice our stop data for development
+    // TODO: Remove dev edit
+    // stopInfoArr = stopInfoArr.slice(0, 10);
+
+    // Recenter our map
+    this.map.panTo([stopInfoArr[0].stop_lat, stopInfoArr[0].stop_lon]);
+
+    // Draw dots for each of our stations
+    // TODO: Make stations debuggable so group them in a layer or something
+    var that = this;
+    stopInfoArr.forEach(function generateStopMarker (stopInfo) {
+      // Generate our marker
+      // [{stop_id: '98', stop_code: '198', stop_name: '2ND ST ...', stop_desc: ' ',
+      //   stop_lat: '37...', stop_lon: '-122...', zone_id: ' ', stop_url: ' '}, ...]
+      // to '2ND ST ... (id: 98, code: 198)'
+      var popupStr = stopInfo.stop_name + ' (id: ' + escapeHtml(stopInfo.stop_id) + ', ' +
+        'code: ' + escapeHtml(stopInfo.stop_code) + ')';
+      var stopMarker = L.circleMarker([stopInfo.stop_lat, stopInfo.stop_lon], {
+        stroke: false,
+        fillOpacity: 1.0,
+        radius: 3
+      }).bindPopup(popupStr);
+
+      // Bind our marker
+      that.stopMarkersLayer.addLayer(stopMarker);
+    });
+  }
+};
 window.Application = module.exports = Application;
